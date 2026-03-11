2a85dc3dadeaa88214e2c2f548346743482ed14f
Ryan Tsai <ryan@ryantsai.com>
2026-03-11 03:13:42 +0000

    fix: bundle Kenney asset URLs via Vite imports for GitHub Pages

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,16 +1,23 @@
 import Phaser from 'phaser';
+import bikeUrl from '../assets/images/player_bike.png';
+import shopUrl from '../assets/images/poi_shop.png';
+import officeUrl from '../assets/images/poi_office.png';
+import policeUrl from '../assets/images/poi_police.png';
+import customerUrl from '../assets/images/poi_customer.png';
+import bgmUrl from '../assets/audio/bgm_time_driving.ogg';
+import sfxUrl from '../assets/audio/sfx_interact.ogg';
 
 export default class BootScene extends Phaser.Scene {
   constructor() { super('BootScene'); }
 
   preload() {
-    this.load.image('bikeReal', 'src/assets/images/player_bike.png');
-    this.load.image('poiShopReal', 'src/assets/images/poi_shop.png');
-    this.load.image('poiOfficeReal', 'src/assets/images/poi_office.png');
-    this.load.image('poiPoliceReal', 'src/assets/images/poi_police.png');
-    this.load.image('poiCustomerReal', 'src/assets/images/poi_customer.png');
-    this.load.audio('bgmLoop', 'src/assets/audio/bgm_time_driving.ogg');
-    this.load.audio('sfxInteract', 'src/assets/audio/sfx_interact.ogg');
+    this.load.image('bikeReal', bikeUrl);
+    this.load.image('poiShopReal', shopUrl);
+    this.load.image('poiOfficeReal', officeUrl);
+    this.load.image('poiPoliceReal', policeUrl);
+    this.load.image('poiCustomerReal', customerUrl);
+    this.load.audio('bgmLoop', bgmUrl);
+    this.load.audio('sfxInteract', sfxUrl);
   }
 
   create() {
@@ -21,19 +28,9 @@ export default class BootScene extends Phaser.Scene {
     g.fillStyle(0x3f3f46, 1); g.fillRect(0, 0, 64, 64); g.lineStyle(2, 0xf8fafc, 0.9); g.lineBetween(0, 32, 64, 32); g.generateTexture('road-h', 64, 64); g.clear();
     g.fillStyle(0x3f3f46, 1); g.fillRect(0,0,64,64); g.lineStyle(2,0xf8fafc,0.9); g.lineBetween(32,0,32,64); g.lineBetween(0,32,64,32); g.generateTexture('road-x',64,64); g.clear();
     g.fillStyle(0x64748b,1); g.fillRect(0,0,64,64); g.fillStyle(0x94a3b8,1); g.fillRect(8,8,48,48); g.generateTexture('building',64,64); g.clear();
-    g.fillStyle(0xf59e0b,1); g.fillRoundedRect(0,0,64,64,8); g.fillStyle(0x111827,1); g.fillRect(10,28,44,6); g.generateTexture('poi-shop',64,64); g.clear();
-    g.fillStyle(0x22c55e,1); g.fillRoundedRect(0,0,64,64,8); g.fillStyle(0x052e16,1); g.fillRect(10,28,44,6); g.generateTexture('poi-office',64,64); g.clear();
 
-    // bike texture
-    g.fillStyle(0x1d4ed8, 1); g.fillRoundedRect(4, 18, 56, 28, 6);
-    g.fillStyle(0x111827, 1); g.fillCircle(14, 50, 10); g.fillCircle(50, 50, 10);
-    g.fillStyle(0xfbbf24, 1); g.fillRect(50, 22, 8, 6);
-    g.generateTexture('bike', 64, 64); g.clear();
-
-    // rain pixel
     g.fillStyle(0x93c5fd, 1); g.fillRect(0, 0, 2, 8); g.generateTexture('rain', 2, 8); g.clear();
 
-    this.textures.renameTexture('bike','bikeFallback');
     this.scene.start('WorldScene');
   }
 }
